7a6f543a5430a367f51b1a267f4d15a5dd7222b5
joelee2017 <33776374+joelee2017@users.noreply.github.com>
2019-12-23 14:56:16 +0800

    Add files via upload

---
+++ LINQToXML/LINQToXML/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace LINQToXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = System.IO.Directory.GetCurrentDirectory();
+            // XElement xelement = XElement.Load(@"Test.xml", LoadOptions.SetBaseUri | LoadOptions.SetLineInfo);
+            XElement xelement = XElement.Load(@"..\..\..\Test.xml");
+
+            var fileSettings = from filexml in xelement.Descendants()
+                               select filexml;
+            try
+            {
+                Console.WriteLine("------------FirstNode---------------");
+                foreach (XElement xEle in fileSettings)
+                {
+                    Console.WriteLine(xEle.Name + ": " + xEle.FirstNode);
+                }
+                Console.WriteLine("----------------------------------------");
+
+
+                Console.WriteLine("------------Value---------------");
+                foreach (XElement xEle in fileSettings)
+                {
+                    Console.WriteLine(xEle.Name + ": " + xEle.Value);
+                }
+                Console.WriteLine("----------------------------------------");
+
+
+                Console.WriteLine("------------Attribute---------------");
+                foreach (XElement xEle in fileSettings)
+                {
+                    Console.WriteLine(xEle.Name + ": " + xEle.Attribute("Attr")?.Value);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
